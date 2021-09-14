34a961ef27f360dc2dad79c6a69e8d84c8bbcdde
BrendonIrwan <ed7-aspire4925@hotmail.com>
2021-09-14 00:54:30 +0000

    Update 3 Revision 3

--- src/calc.sh
+++ src/calc.sh
@@ -1,56 +1,68 @@
 #!/bin/bash
 
+# Undo stack size.
 exl=4
+# Prompt string.
 pst='calc> '
 
+# Prints header.
 echd() {
-  echo -e 'Calc u3r2 by Brendon, 05/16/2021.
+  echo -e 'Calc u3r3 by Brendon, 09/13/2021.
 —An undependable calculator. https://github.com/ed7n/calc\n'
 }
 
+# Prints usage.
 ecus() {
   echo 'Usage: [<expression>]'
 }
 
+# Clears expression, and answer with a truthy `$1`.
 clr() {
   exin
   ex[$exj]=
   [[ "$1" ]] && { ans=0 ; echo 'All Cleared.' ; } || echo 'Expression cleared.'
 }
 
+# Returns field `$2` of `$1` delimited by `/`.
 cutd() {
   cuts "$1" '/' "$2"
 }
 
+# Returns field `$2` of `$1` delimited by `.`.
 cutf() {
   cuts "$1" '.' "$2"
 }
 
+# Returns field `$3` of `$1` delimited by `$2`.
 cuts() {
   echo -n "$1" | cut -d "$2" -f "$3"
 }
 
+# Divide `$1` by `$2` with fractional precision.
 div() {
   (( $2 )) || { echo 'Division by zero.' ; return 1 ; }
   acc=$(( $1 % $2 ))
-  [[ $(( acc * 10 )) == "$acc"'0' ]] || { echo "$(( $1 / $2 ))" ; return ; }
+  [[ $(( acc * 10 )) == "$acc"'0' ]] || { out=$(( $1 / $2 )) ; return ; }
   rg0=
   rg1=
-  while [[ $(( acc * 10 )) == "$acc"'0' ]]; do
+  while true; do
     (( acc *= 10 ))
     (( acc < $2 )) && rg1=$rg1'0'
+    [[ $(( acc * 10 )) == "$acc"'0' ]] || break
   done
   (( acc /= $2 ))
   (( acc < 0 )) && { (( acc *= -1 )) ; (( $1 < $2 )) && rg0=- ; }
   out=$rg0$(( $1 / $2 )).$rg1$acc
 }
 
+# Evaluates expression and prints its result, and saves them with a truthy `$1`.
 evl() {
   exev || return
   [[ "$1" ]] && { ans=$(cutf "$out" '1') ; exin ; ex[$exj]= ; }
   echo ' ans: '"$out"
 }
 
+# Concatenates input to expression.
 excn() {
   ins "$inp"
   [[ "${ex[$exj]}" ]] && { [[ "$pre" == '1' ]] \
@@ -61,10 +73,12 @@ excn() {
   ex[$exj]=$acc
 }
 
+# Decrements undo stack cursor index.
 exdc() {
   [[ "$exj" == '0' ]] && exj=$(( exl - 1 )) || (( exj-- ))
 }
 
+# Evaluates expression to output.
 exev() {
   [[ "$fps" == '1' ]] \
       && [[ "$(exp)" == *([[:space:]])*(-)+([[:digit:]])*([[:space:]])/*([[:space:]])*(-)+([[:digit:]])*([[:space:]]) ]] \
@@ -72,17 +86,21 @@ exev() {
       || out=$(echo $(( $(exp) )))
 }
 
+# Increments undo stack cursor index, and head and tail indeces with a falsy
+# `$1`.
 exin() {
-  [[ "$1" ]] && { exj=$(( (exj + 1) % exl )) ; return ; }
   exj=$(( (exj + 1) % exl ))
+  [[ "$1" ]] && return
   exk=$exj
   (( exi == exk )) && exi=$(( (exi + 1) % exl ))
 }
 
+# Echoes expression.
 exp() {
   echo -n "${ex[$exj]}"
 }
 
+# Toggles fractional precision for simple divisions.
 fps() {
   shopt -q 'extglob' || shopt -s 'extglob' \
       || { echo 'The `extglob` shell option can not be set.' ; return ; }
@@ -93,6 +111,7 @@ fps() {
   echo '.'
 }
 
+# Appends key `$1` to input.
 ins() {
   case "$1" in
     ans ) inp=$ans ;;
@@ -104,6 +123,7 @@ ins() {
   echo "$pst$inp"
 }
 
+# Prints calculator state, and all other program variables with a truthy `$1`.
 mon() {
   [[ "$1" ]] || echo ' exp: '"$(exp)"
   echo ' ans: '"$ans"'
@@ -125,6 +145,7 @@ mon() {
  rg1: '"$rg1"
 }
 
+# Toggles input prepending.
 pre() {
   echo -n 'Subsequent inputs will be '
   [[ "$pre" == '1' ]] \
@@ -133,6 +154,7 @@ pre() {
   echo ' to the expression.'
 }
 
+# Redoes last change to expression.
 rdo() {
   [[ "$exj" == "$exk" ]] \
       && { echo 'Reached the top of the undo stack.' ; return ; }
@@ -140,6 +162,7 @@ rdo() {
   echo 'Redid last change to expression.'
 }
 
+# Prints reference.
 ref() {
   echo '——Functions
 Enter these individually in a separate input.
@@ -200,11 +223,13 @@ Numerical Notation
   012   base-8  "octal"'
 }
 
+# Saves last result to memory.
 sto() {
   mem=$ans
   echo ' mem: '"$mem"
 }
 
+# Undoes last change to expression.
 udo() {
   [[ "$exj" == "$exi" ]] \
       && { echo 'Reached the bottom of the undo stack.' ; return ; }
@@ -213,16 +238,27 @@ udo() {
 }
 
 [[ "$1" == "--help" ]] && { echd ; ecus ; exit 0 ; }
+# Accumulator.
 acc=
+# Answer.
 ans=0
+# Undo stack tail index.
 exi=0
+# Undo stack cursor index.
 exj=0
+# Undo stack head index.
 exk=0
+# Fractional precision flag.
 fps=0
+# Input.
 inp=
+# Memory.
 mem=0
+# Output.
 out=
+# Input prepending flag.
 pre=0
+# Registers
 rg0=
 rg1=
 until [[ "$exj" == "$exl" ]]; do
